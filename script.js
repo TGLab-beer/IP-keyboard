ae4e418b91edfb03a71a147753f2e1f2af8d86f9
TGL-mac <50290283+TGLab-beer@users.noreply.github.com>
2023-12-17 03:06:16 +0300

    пиздато
    короче теперь есть логика, можно печатать, если допустил ошибку то можно только стереть, убрал к чертовой матери смену языка, короче по кайфу

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const texts = ["Великолепная зеленая трава шуршит под легким ветерком. Пение птиц наполняет воздух свежестью и живостью. Солнце ярко светит, даря свои теплые лучи всему вокруг. Вдали виднеется голубое небо, усыпанное белыми пушистыми облаками. Здесь, среди природы, человек чувствует себя особенным, един с окружающим миром. Листва деревьев шелестит, создавая неповторимую мелодию лесной гармонии. Воздух наполнен ароматами цветов и трав, приглашая каждого насладиться запахами природы. Жизнь кипит вокруг, каждый организм стремится к собственной нише в этом удивительном эко-системе. В этом мире есть место для каждого, место, где каждый может найти свое счастье и покой. Человек находит здесь вдохновение, силу и баланс, очищая свой разум и душу. А все вокруг напоминает о чудесах жизни, о том, как велик и непостижим ее круговорот.",
 "В часах хаоса и раздоров, когда тропинки души запутались в клубках нежданной судьбы, важно помнить о ценности мгновений. Ведь каждый вздох, каждый взгляд, каждое слово — частичка необратимой жизни. Стоит лишь остановиться, взглянуть вокруг и проникнуться этой закрученной мозаикой реальности. В истерзанном мире суеты и потерь настоящее словно мерцает звездой в бескрайнем космосе. И все мы — лишь крошечные песчинки на галактическом песке времени. Однако в этой многоликой вселенной каждый из нас несет в себе свою уникальную искру, способную осветить тьму и согреть холодные сердца. Мы — странники в бескрайних просторах, изучающие азбуку жизни и любви, непрерывно познающие и переживающие нелегкие испытания. Пусть мы идем сквозь бурю и грозы, но в конце этого пути светит надежда и возможность переосмыслить свою жизнь. Ведь каждый миг — это новая страница в нашей истории, и каждый шаг наполнен бесконечными возможностями."]
     var correctText = "";
-
+    var state = "ready";
+    var isError = false;
+    var timer = 0;
+    function changeState(newState){
+      if(newState === "start" && state === "ready"){
+        startTest();
+        state = "start";
+      }
+    }
 
     function startTest() {
       var input = document.getElementById("input");
@@ -10,31 +18,52 @@ const texts = ["Великолепная зеленая трава шуршит 
       input.focus();
       document.getElementById("error-msg").innerHTML = "";
 
-      var timer = document.getElementById("timer");
       var result = document.getElementById("result");
-      var words = text.split(" ").length;
-      var startTime = Date.now();
+      var words = texts.length;
 
-      setTimeout(function() {
-        var elapsedTime = (Date.now() - startTime) / 1000 / 60; // в минутах
+      setInterval(function() {
+        //var elapsedTime = (Date.now() - startTime) / 1000 / 60; // в минутах
         var typedWords = input.value.trim().split(" ").length;
-        if (input.value.trim() === correctText.trim()) {
-          var speed = typedWords / elapsedTime;
-          result.innerHTML = "Вы набрали " + typedWords + " слов в минуту.";
+        if (input.value[input.value.length - 1] === texts[0][input.value.length - 1]) {
+          var speed = typedWords / timer;
+          
+          input.style.color = 'black';
+          input.readOnly = false;
+          isError = false;
         } else {
           result.innerHTML = "";
-          document.getElementById("error-msg").innerHTML = "Вы набрали неправильный текст.";
-        }
-        input.disabled = true;
-      }, 60000);
+          
+          input.readOnly = true;
+          isError = true;
 
-      // Генерирует новый случайный текст
-      correctText = text.split(" ").sort(function() {
-        return 0.5 - Math.random();
-      }).slice(0, 200).join(" ");
-      document.getElementById("text").innerHTML = correctText;
+          var inputElement = input;
+          var lastCharIndex = input.length - 1;
+          inputElement.setSelectionRange(lastCharIndex, lastCharIndex + 1);
+          inputElement.style.color = 'red';
+        }
+        
+      }, 10);
+
+      
+      document.getElementById("text").innerHTML = texts[0];
     }
 
+    function handleKeyDown(event) {
+      if(isError){
+        console.log("trying to change");
+        input = document.getElementById("input");
+      if (event.keyCode === 8) {
+          event.preventDefault();
+          console.log("backspace");
+  
+          if (input.value.length > 0) {
+              var newValue = input.value.slice(0, -1);
+  
+              input.value = newValue;
+          }
+      }
+    }
+  }
     function checkText() {
       var input = document.getElementById("input");
       if (input.value.trim() === correctText.trim()) {
@@ -43,19 +72,17 @@ const texts = ["Великолепная зеленая трава шуршит 
         input.style.border = "2px solid red";
       }
     }
-    (function(){
-      let timeLeft = 0;
-          function updateTimer() {
-        document.getElementById('timer').textContent = timeLeft;
+
+    function incrementTimer() {
+      if (state === "start") {
+        timer++;
+        updateTimer();
       }
-      function countdown() {
-        if (timeLeft > -1) {
-          timeLeft++;
-          updateTimer();
-        } else {
-          clearInterval(timer);
-          alert('Время вышло!');
-        }
-      }
-      let timer = setInterval(countdown, 1000);
-      })();
+    }
+    function updateTimer() {
+      document.getElementById('timer').textContent = "Время:" + timer;
+    }
+    setInterval(incrementTimer, 1000);
+
+
+    
